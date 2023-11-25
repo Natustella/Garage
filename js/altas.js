1d6dea5e6f9104ff9458d141aaff21bc53e4864f
Natustella <natustella@gmail.com>
2023-11-25 19:53:12 -0300

    CRUD

---
+++ js/altas.js
@@ -0,0 +1,34 @@
+const URL = "http://127.0.0.1:5000/" 
+// Capturamos el evento de envío del formulario 
+document.getElementById('formulario').addEventListener('submit', function (event) {
+    event.preventDefault(); // Evitamos que se envie el form 
+    var formData = new FormData(); 
+    formData.append('codigo', document.getElementById('codigo').value);
+	formData.append('nombre', document.getElementById('nombre').value); 
+    formData.append('descripcion', document.getElementById('descripcion').value); 
+    formData.append('precio', document.getElementById('precio').value); ; 
+    formData.append('imagen', document.getElementById('imagenProducto').files[0]);
+    
+    fetch(URL + 'productos', { 
+        method: 'POST', 
+        body: formData // Aquí enviamos formData en lugar de JSON 
+    }) 
+	    .then(function (response) {  
+		    if (response.ok) { 
+			    return response.json(); 
+			}  
+		}) 
+		.thenthen(function (data) { 
+		    alert('Producto agregado correctamente.');
+			// Limpiar el formulario para el proximo producto 
+			document.getElementById('codigo').value = "";
+			document.getElementById('nombre').value = ""; 
+			document.getElementById('descripcion').value = ""; 
+			document.getElementById('precio').value = ""; 
+			document.getElementById('imagen').value = ""; 
+		})
+		.catch(function (error) { 
+		    // Mostramos el error, y no limpiamos el form. 
+		    alert('Error al agregar el producto.'); 
+		});
+	})
---
+++ js/modificaciones.js
@@ -0,0 +1,66 @@
+const URL = "http://127.0.0.1:5000/"
+
+const app = Vue.createApp({
+    data() {
+        return {
+            codigo: '',
+            nombre: '',
+            descripcion: '',
+            precio: '',
+            imagen: '',
+            imagenUrlTemp: null,
+            mostrarDatosProducto: false,
+        };
+    }, methods: {
+        obtenerProducto() {
+            fetch(URL + 'productos/' + this.codigo)
+                .then(response => response.json())
+                .then(data => {
+                    this.nombre = data.nombre;
+                    this.descripcion = data.descripcion;
+                    this.precio = data.precio;
+                    this.imagen = data.imagen;
+                    this.mostrarDatosProducto = true;
+                }).catch(error => console.error('Error:', error));
+        },
+        seleccionarImagen(event) {
+            const file = event.target.files[0];
+            this.imagenSeleccionada = file;
+            this.imagenUrlTemp = URL.createObjectURL(file); // Crea una URL temporal para la vista previa 
+        }, guardarCambios() {
+            const formData = new FormData();
+            formData.append('codigo', this.codigo);
+            formData.append('nombre', this.nombre);
+            formData.append('descripcion', this.descripcion);
+            formData.append('precio', this.precio);
+            if (this.imagenSeleccionada) {
+                formData.append('imagen', this.imagenSeleccionada, this.imagenSeleccionada.name);
+            }
+
+            fetch(URL + 'productos/' + this.codigo, {
+                method: 'PUT',
+                body: formData,
+            })
+                .then(response => response.json())
+                .then(data => {
+                    alert('Producto actualizado correctamente');
+                    this.limpiarFormulario();
+                }).catch(error => {
+                    console.error('Error:', error);
+                    alert('Error al actualizar el producto');
+                });
+        },
+        limpiarFormulario() {
+            this.codigo = '';
+            this.nombre = '';
+            this.descripcion = '';
+            this.precio = '';
+            this.imagen = '';
+            this.imagenSeleccionada = null;
+            this.imagenTemp = null;
+            this.mostrarDatosProducto = false;
+        }
+    }
+});
+
+app.mount('#app');
